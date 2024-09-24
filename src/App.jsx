b9cac31d2d11fcee18e171b2887c08bfc8657e63
sameeraherath <sameerastar6@gmail.com>
2024-09-24 15:07:53 +0530

    Add App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import AboutSection from "./components/AboutSection";
+import RoomsSection from "./components/RoomsSection";
+import GallerySection from "./components/GallerySection";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <section id="home">
+          <HeroSection />
+        </section>
+        <section id="about">
+          <AboutSection />
+        </section>
+        <section id="rooms">
+          <RoomsSection />
+        </section>
+        <section id="gallery">
+          <GallerySection />
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
